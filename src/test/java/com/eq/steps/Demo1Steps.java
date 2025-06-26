91a6222deb5450ff016dd81c2907f6657843e08f
balaji-githubstore <balajidinakaran1@gmail.com>
2025-06-26 09:48:35 +0530

    Added Scenario Outline and Background

--- src/test/java/com/eq/steps/Demo1Steps.java
+++ src/test/java/com/eq/steps/Demo1Steps.java
@@ -5,25 +5,34 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class Demo1Steps {
-	
+
 	@Given("I have browser with OrangeHRM application")
 	public void i_have_browser_with_orange_hrm_application() {
-	    System.out.println("given");
+		System.out.println("given");
 	}
+
 	@When("I enter username as {string}")
 	public void i_enter_username_as(String username) {
-		 System.out.println("when"+username);
+		System.out.println("when" + username);
 	}
+
 	@When("I enter password as {string}")
 	public void i_enter_password_as(String password) {
-		System.out.println("when"+password);
+		System.out.println("when" + password);
 	}
+
 	@When("I click on login")
 	public void i_click_on_login() {
 		System.out.println("when login");
 	}
+
 	@Then("I should get access to dashboard with content as {string}")
 	public void i_should_get_access_to_dashboard_with_content_as(String expectedValue) {
-		System.out.println("then"+expectedValue);
+		System.out.println("then" + expectedValue);
+	}
+
+	@Then("I should not get access to portal with error {string}")
+	public void i_should_not_get_access_to_portal_with_error(String expectedError) {
+		System.out.println("then " + expectedError);
 	}
 }
--- src/test/resources/feature/Login.feature
+++ src/test/resources/feature/Login.feature
@@ -3,16 +3,22 @@ Feature: Login
   As an admin
   I want to login into the orangehrm portal
 
-  Scenario: Valid Login
+  Background: 
     Given I have browser with OrangeHRM application
+
+  Scenario: Valid Login
     When I enter username as "Admin"
     And I enter password as "admin123"
     And I click on login
     Then I should get access to dashboard with content as "Time at Work"
 
-  Scenario: Invalid Login
-    Given I have browser with OrangeHRM application
-    When I enter username as "john"
-    And I enter password as "john123"
+  Scenario Outline: Invalid Login
+    When I enter username as "<username>"
+    And I enter password as "<password>"
     And I click on login
-    Then I should not get access to portal with error "Invalid credentials"
+    Then I should not get access to portal with error "<expected_error>"
+
+    Examples: 
+      | username | password | expected_error      |
+      | saul     | saul123  | Invalid credentials |
+      | kim      | kim123   | Invalid credentials |
